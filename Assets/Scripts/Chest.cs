152c3d70c5b3cab8c4b9194c74f0b7611d15e1f0
Maijs Garais <maireadsbooks@gmail.com>
2022-09-17 19:23:52 +0100

    add chest collection logic and sprite, make money chests into a prefab

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chest : Collectable {
+    public Sprite emptyChestSprite;
+    public int pesosAmount;
+
+    protected override void Start() {
+        base.Start();
+        pesosAmount = Random.Range(5, 11);
+    }
+
     protected override void OnCollect() {
         if (!isCollected) {
             base.OnCollect();
-            Debug.Log("Give pesos");
+            GetComponent<SpriteRenderer>().sprite = emptyChestSprite;
+            Debug.Log("Give " + pesosAmount + " pesos");
         }
     }
 }
