52009931819a5f1a1850ba03bdd2109979346c31
Maijs Garais <maireadsbooks@gmail.com>
2022-09-17 21:04:59 +0100

    add state save/load logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
+    public static readonly string SAVE_PESOS = "Pesos";
+    public static readonly string SAVE_SKIN_PREF = "Skin";
+    public static readonly string SAVE_XP = "XP";
+    public static readonly string SAVE_WEAPON_LVL = "WeaponLvl";
+    public static readonly string SAVE_EXISTS = "Saved";
 
     private void Awake() {
         // if instange of game manager already exists, destroy the new
@@ -37,10 +42,23 @@ public class GameManager : MonoBehaviour {
 
     // save state
     public void SaveState() {
-        Debug.Log("Save State");
+        // save state to player prefs
+        PlayerPrefs.SetInt(SAVE_SKIN_PREF, 0);
+        PlayerPrefs.SetInt(SAVE_PESOS, pesos);
+        PlayerPrefs.SetInt(SAVE_XP, xp);
+        PlayerPrefs.SetInt(SAVE_WEAPON_LVL, 0);
+        // extra key to assert that some data is saved
+        PlayerPrefs.SetInt(SAVE_EXISTS, 1);
     }
 
     public void LoadState(Scene scene, LoadSceneMode mode) {
-        Debug.Log("Load State");
+        // if no save data exists, theres nothing to load
+        if (!PlayerPrefs.HasKey(SAVE_EXISTS))
+            return;
+
+        // skin = prefs load
+        pesos = PlayerPrefs.GetInt(SAVE_PESOS);
+        xp = PlayerPrefs.GetInt(SAVE_XP);
+        // weapon = prefs load
     }
 }
