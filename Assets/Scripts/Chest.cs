d94d17a8075947f910720227cff5c827abe84c24
Maijs Garais <maireadsbooks@gmail.com>
2022-09-21 19:46:07 +0100

    fix crate sorting

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chest : Collectable {
+    // TODO: give chests a chance to be mimics.
     public Sprite emptyChestSprite;
     public int pesosAmount;
 
