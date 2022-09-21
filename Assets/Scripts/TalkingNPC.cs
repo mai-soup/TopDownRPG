f13b7a7297fce496d3296e372713bff5b139c8cf
Maijs Garais <maireadsbooks@gmail.com>
2022-09-21 14:41:24 +0100

    make NPC talk, adjust floating text prefab size

---
+++ Assets/Scripts/TalkingNPC.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingNPC : Collidable {
+    public string msg;
+    private float lastTriggered;
+    private static readonly float triggerCooldown = 0.1f;
+    protected override void OnCollide(Collider2D collider) {
+        if (Time.time - lastTriggered > triggerCooldown) {
+            lastTriggered = Time.time;
+            GameManager.instance.ShowText(
+                msg,            // txt to show
+                16,             // fontsize
+                Color.white,    // colour
+                transform.position + // show above npc
+                    new Vector3(
+                        0, 2*GetComponent<BoxCollider2D>().bounds.extents.y, 0),
+                Vector3.zero,   // no motion
+                triggerCooldown
+            );
+        }
+    }
+}
